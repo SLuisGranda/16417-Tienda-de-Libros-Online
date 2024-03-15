ff2ba744fa111537d426d05d4f441dddb500c296
Granda Mena Sigifredo Luis <135286409+SLuisGranda@users.noreply.github.com>
2024-03-15 10:16:01 -0500

    Update Usuario.java

--- Proyecto Tienda de Libros Online/2.Código/2.2 Modelos/Usuario.java
+++ Proyecto Tienda de Libros Online/2.Código/2.2 Modelos/Usuario.java
@@ -1,1 +1,84 @@
+package Modelos;
+
+/**
+ *
+ * @author User
+ */
+public class Usuario {
+
+    int id;
+    int documento;
+    String nombre, correo, password, rol, estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, int documento, String nombre, String correo, String password, String rol, String estado) {
+        this.id = id;
+        this.documento = documento;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+   
+}
 
